c07785a8e69a3b00a8af6a6ede02b5a79e6e0648
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2023-03-08 17:12:03 +0200

    feat: add unit tests

--- Tests/UnitTest.cs
+++ Tests/UnitTest.cs
@@ -1,40 +1,47 @@
 using Main;
+using Main.classes;
 using Xunit;
 
 namespace Tests;
 
 public static class UnitTest {
-	// private static readonly Grammar         Grammar         = new(Program.Vn, Program.Vt, Program.P, "S");
-	// private static readonly FiniteAutomaton FiniteAutomaton = Grammar.ToFiniteAutomaton();
-	//
-	// [Fact]
-	// private static void TestGrammar() {
-	// 	for (var i = 0; i < 50; i++) {
-	// 		var str = Grammar.GenerateString();
-	//
-	// 		Assert.NotNull(str);
-	// 		Assert.NotEmpty(str);
-	// 		Assert.IsType<string>(str);
-	// 	}
-	// }
-	//
-	// [Theory]
-	// [InlineData("abcb",    true)]
-	// [InlineData("ee",      false)]
-	// [InlineData("asfsdgf", false)]
-	// [InlineData("112345",  false)]
-	// private static void TestAutomatonPredefined(string str, bool expected) {
-	// 	Assert.Equal(expected, FiniteAutomaton.StringBelongToLanguage(str));
-	// }
-	//
-	// [Fact]
-	// private static void TestAutomatonGenerated() {
-	// 	for (int i = 0; i < 50; i++) {
-	// 		Assert.True(
-	// 			FiniteAutomaton.StringBelongToLanguage(
-	// 				Grammar.GenerateString()
-	// 			)
-	// 		);
-	// 	}
-	// }
+	private static readonly Grammar Grammar = new(
+		Program.Aphabet, 
+		Program.NrOfStates, 
+		Program.FinalStates,
+		Program.Transitions
+		);
+
+	private static readonly FiniteAutomaton FiniteAutomaton = Grammar.ToFiniteAutomaton();
+
+	[Fact]
+	private static void TestGrammar() {
+		for (var i = 0; i < 50; i++) {
+			var str = Grammar.GenerateString();
+
+			Assert.NotNull(str);
+			Assert.NotEmpty(str);
+			Assert.IsType<string>(str);
+		}
+	}
+
+	[Theory]
+	[InlineData("bcabbcabbcabcabc",    true)]
+	[InlineData("ee",      false)]
+	[InlineData("asfsdgf", false)]
+	[InlineData("112345",  false)]
+	private static void TestAutomatonPredefined(string str, bool expected) {
+		Assert.Equal(expected, FiniteAutomaton.StringBelongToLanguage(str));
+	}
+
+	[Fact]
+	private static void TestAutomatonGenerated() {
+		for (int i = 0; i < 50; i++) {
+			Assert.True(
+				FiniteAutomaton.StringBelongToLanguage(
+					Grammar.GenerateString()
+				)
+			);
+		}
+	}
 }
